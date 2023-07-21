ff3b96175f53c4019f150d50c079f0b05ff62b17
Frontenddevkan <frontenddevkan@gmail.com>
2023-07-21 19:04:38 +0500

    fix: fix gulp

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -8,9 +8,9 @@ export function isWebp(){
         };
         webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
         }
-        
+
         testWebP(function (support) {
-        
+
             if (support == true) {
             document.querySelector('body').classList.add('webp');
             }else{
@@ -75,7 +75,7 @@ const swiper = new Swiper('.swiper', {
     direction: 'horizontal',
     slidesPerView: 3,
     loop: true,
-  
+
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
